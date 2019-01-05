5e86fcf3d067dd507b2896b8a8ce5eb109e50db5
kmecpp <kmecpp@gmail.com>
2019-01-05 17:47:26 -0500

    More work on inventories and items

--- src/main/java/com/kmecpp/osmium/api/inventory/InventoryGUI.java
+++ src/main/java/com/kmecpp/osmium/api/inventory/InventoryGUI.java
@@ -2,13 +2,18 @@ package com.kmecpp.osmium.api.inventory;
 
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
+import org.spongepowered.api.block.BlockTypes;
+import org.spongepowered.api.item.ItemTypes;
 
 public class InventoryGUI {
 	
 	{
+		ItemStack bi;
 		org.spongepowered.api.item.inventory.ItemStack i;
 		Material m;
-		m.is
+		ItemTypes types;
+//		org.spongepowered.api.item.inventory.ItemStack.of(itemType, quantity)
+//		i.getType().getisSpecific()
 	}
 
 }
--- src/main/java/com/kmecpp/osmium/api/inventory/ItemStack.java
+++ src/main/java/com/kmecpp/osmium/api/inventory/ItemStack.java
@@ -4,10 +4,25 @@ import com.kmecpp.osmium.api.Abstraction;
 
 public interface ItemStack extends Abstraction {
 
-	//	String getId();
-	
-	
+	//	private ItemType type;
+	//	private int amount;
+	//
+	//	public ItemType getType() {
+	//		return type;
+	//	}
+	//
+	//	public int getAmount() {
+	//		return amount;
+	//	}
+	//
+	//	public boolean isEmpty() {
+	//		return amount == 0;
+	//	}
 
+	//	String getId();
+	//
+	//	ItemType getType();
+	//
 	String getDisplayName();
 
 	int getAmount();
