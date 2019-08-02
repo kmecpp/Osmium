059d4bbf40d875ff7bce5d5ba508dcc624187325
kmecpp <kmecpp@gmail.com>
2019-08-02 15:25:03 -0400

    Make hook loading lazy

--- src/main/java/com/kmecpp/osmium/Hook.java
+++ src/main/java/com/kmecpp/osmium/Hook.java
@@ -6,12 +6,24 @@ import org.bukkit.Bukkit;
 import org.spongepowered.api.Sponge;
 import org.spongepowered.api.plugin.PluginContainer;
 
+import com.kmecpp.osmium.api.logging.OsmiumLogger;
+import com.kmecpp.osmium.api.plugin.OsmiumPlugin;
+import com.kmecpp.osmium.api.util.Reflection;
+import com.kmecpp.osmium.api.util.StringUtil;
+
 public class Hook<T> {
 
 	private T hook;
+	private Callable<T> loader;
+	private boolean loaded;
 
 	private Hook(T hook) {
 		this.hook = hook;
+		this.loaded = true;
+	}
+
+	private Hook(Callable<T> loader) {
+		this.loader = loader;
 	}
 
 	public static <T> Hook<T> of(T inst) {
@@ -22,53 +34,70 @@ public class Hook<T> {
 		return new Hook<>(null);
 	}
 
+	public void ensureLoaded() {
+		if (!loaded) {
+			try {
+				this.hook = loader.call();
+				this.loader = null;
+			} catch (Throwable t) {
+				t.printStackTrace();
+			}
+			loaded = true;
+		}
+	}
+
 	public boolean isLoaded() {
+		ensureLoaded();
 		return hook != null;
 	}
 
 	public T get() {
+		ensureLoaded();
 		return hook;
+	}
+
+	public static <T> Hook<T> get(Callable<T> loader) {
+		return new Hook<>(loader);
 	}
 
 	@SuppressWarnings("unchecked")
 	public static <T> T get(String pluginName) {
-		Object result = null;
-		if (Platform.isBukkit()) {
-			result = Bukkit.getPluginManager().getPlugin(pluginName);
-		} else if (Platform.isSponge()) {
-			for (PluginContainer pluginContainer : Sponge.getPluginManager().getPlugins()) {
-				if (pluginContainer.getName().equalsIgnoreCase(pluginName) || pluginContainer.getId().equalsIgnoreCase(pluginName)) {
-					result = pluginContainer.getInstance().orElse(null);
+		OsmiumPlugin callingPlugin = Osmium.getPlugin(Reflection.getInvokingClass(1));
+		if (!StringUtil.contains(pluginName, callingPlugin.getDependencies())) {
+			OsmiumLogger.warn(callingPlugin.getName() + " is attempting to hook into " + pluginName + " but it is not specified as a dependency");
+		}
+
+		return (T) new Hook<>(() -> {
+			if (Platform.isBukkit()) {
+				return Bukkit.getPluginManager().getPlugin(pluginName);
+			} else if (Platform.isSponge()) {
+				for (PluginContainer pluginContainer : Sponge.getPluginManager().getPlugins()) {
+					if (pluginContainer.getName().equalsIgnoreCase(pluginName) || pluginContainer.getId().equalsIgnoreCase(pluginName)) {
+						return pluginContainer.getInstance().orElse(null);
+					}
 				}
 			}
-		}
-		return (T) new Hook<>(result);
+			return null;
+		});
 	}
 
 	@SuppressWarnings("unchecked")
 	public static <T> T getFrom(String method) {
-		String[] parts = method.split("\\:");
-		if (parts.length != 2) {
-			throw new IllegalArgumentException("Expected: class:method");
-		}
-		String className = parts[0];
-		String methodName = parts[1];
+		return (T) new Hook<>(() -> {
+			String[] parts = method.split("\\:");
+			if (parts.length != 2) {
+				throw new IllegalArgumentException("Expected: class:method");
+			}
+			String className = parts[0];
+			String methodName = parts[1];
 
-		try {
-			return (T) new Hook<>(Class.forName(className).getMethod(methodName).invoke(null));
-		} catch (Exception e) {
-			//			e.printStackTrace();
-			return (T) new Hook<>(null);
-		}
-	}
-
-	public static <T> Hook<T> get(Callable<T> callable) {
-		System.out.println("GETTING!!!");
-		try {
-			return new Hook<T>(callable.call());
-		} catch (Throwable t) {
-			return new Hook<T>(null);
-		}
+			try {
+				return Class.forName(className).getMethod(methodName).invoke(null);
+			} catch (Exception e) {
+				e.printStackTrace();
+				return null;
+			}
+		});
 	}
 
 }
