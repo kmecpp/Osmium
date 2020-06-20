ac93a68669cbeb08157a3f97d4f7baf0e27a893f
kmecpp <kmecpp@gmail.com>
2020-06-20 13:56:17 -0400

    More messaging methods

--- src/main/java/com/kmecpp/osmium/api/command/Messageable.java
+++ src/main/java/com/kmecpp/osmium/api/command/Messageable.java
@@ -1,6 +1,7 @@
 package com.kmecpp.osmium.api.command;
 
-import java.util.List;
+import java.util.Collection;
+import java.util.function.Function;
 
 import org.bukkit.ChatColor;
 
@@ -23,14 +24,38 @@ public interface Messageable {
 		send("");
 	}
 
-	default void sendList(String title, List<?> list) {
+	default void sendList(String title, Collection<?> list) {
 		sendList(CS.XEA, title, list);
 	}
 
-	default void sendList(CS colors, String title, List<?> list) {
+	default void sendNumberedList(String title, Collection<?> list) {
+		sendNumberedList(CS.XEA, title, list);
+	}
+
+	default <T> void sendList(String title, Collection<T> list, Function<T, String> prefixer) {
+		sendList(CS.XEA, title, list, prefixer);
+	}
+
+	default void sendList(CS colors, String title, Collection<?> list) {
 		sendTitle(colors, title);
 		for (Object item : list) {
 			sendMessage(colors.getSecondary() + " - " + colors.getPrimary() + item);
+		}
+	}
+
+	default <T> void sendList(CS colors, String title, Collection<T> list, Function<T, String> prefixer) {
+		sendTitle(colors, title);
+		for (T item : list) {
+			sendMessage(colors.getSecondary() + " - " + colors.getPrimary() + prefixer.apply(item) + item);
+		}
+	}
+
+	default void sendNumberedList(CS colors, String title, Collection<?> list) {
+		sendTitle(colors, title);
+		int i = 1;
+		for (Object item : list) {
+			sendMessage("" + colors.getSecondary() + i + ") " + colors.getPrimary() + item);
+			i++;
 		}
 	}
 
