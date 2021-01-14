b3c8a0f44a04e628368022da6555e1d778a5a5dc
kmecpp <kmecpp@gmail.com>
2021-01-14 15:20:45 -0500

    Remove DatabaseSerialization

--- src/main/java/com/kmecpp/osmium/api/persistence/DatabaseSerialization.java
+++ src/main/java/com/kmecpp/osmium/api/persistence/DatabaseSerialization.java
@@ -1,32 +1,32 @@
-package com.kmecpp.osmium.api.persistence;
-
-import java.util.HashMap;
-
-public class DatabaseSerialization {
-
-	private static final HashMap<Class<?>, SerializationData<?>> database = new HashMap<>();
-
-	static {
-		//		register(boolean.class, DBType.BOOLEAN, b -> b ? 1 : 0, deserializer);
-		//		register(Boolean.class, DBType.BOOLEAN, b -> b ? 1 : 0, );
-	}
-
-	public static <T> void register(Class<T> cls, Serializer<T> serializer, Deserializer<T> deserializer) {
-		database.put(cls, new SerializationData<>(true, serializer, deserializer));
-	}
-
-	public static <T> String serialize(T obj) {
-		if (obj == null) {
-			return "null";
-		}
-
-		@SuppressWarnings("unchecked")
-		SerializationData<T> data = (SerializationData<T>) Serialization.getData(obj.getClass());
-		if (data != null) {
-			return data.isCustomType() ? "\"" + data.serialize(obj) + "\"" : data.serialize(obj);
-		} else {
-			return String.valueOf(obj);
-		}
-	}
-
-}
+//package com.kmecpp.osmium.api.persistence;
+//
+//import java.util.HashMap;
+//
+//public class DatabaseSerialization {
+//
+//	private static final HashMap<Class<?>, SerializationData<?>> database = new HashMap<>();
+//
+//	static {
+//		//		register(boolean.class, DBType.BOOLEAN, b -> b ? 1 : 0, deserializer);
+//		//		register(Boolean.class, DBType.BOOLEAN, b -> b ? 1 : 0, );
+//	}
+//
+//	public static <T> void register(Class<T> cls, Serializer<T> serializer, Deserializer<T> deserializer) {
+//		database.put(cls, new SerializationData<>(true, serializer, deserializer));
+//	}
+//
+//	public static <T> String serialize(T obj) {
+//		if (obj == null) {
+//			return "null";
+//		}
+//
+//		@SuppressWarnings("unchecked")
+//		SerializationData<T> data = (SerializationData<T>) Serialization.getData(obj.getClass());
+//		if (data != null) {
+//			return data.isCustomType() ? "\"" + data.serialize(obj) + "\"" : data.serialize(obj);
+//		} else {
+//			return String.valueOf(obj);
+//		}
+//	}
+//
+//}
