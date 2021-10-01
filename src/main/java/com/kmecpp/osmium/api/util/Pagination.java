70f81c7aee0fe25e98d79d2f302a681dd22b4388
kmecpp <kmecpp@gmail.com>
2021-09-30 21:35:01 -0400

    Add Pagination.compute()

--- src/main/java/com/kmecpp/osmium/api/util/Pagination.java
+++ src/main/java/com/kmecpp/osmium/api/util/Pagination.java
@@ -25,4 +25,41 @@ public class Pagination {
 		return (numItems - 1) - getStartIndex(page, pageSize);
 	}
 
+	public static PaginationResult compute(int page, int pageSize, int totalRecords) {
+		return new PaginationResult(page, Pagination.getPageCount(totalRecords, pageSize),
+				Pagination.getStartIndex(page, pageSize), Pagination.getEndIndex(page, pageSize, totalRecords));
+	}
+
+	public static class PaginationResult {
+
+		private final int page;
+		private final int pageCount;
+		private final int startIndex;
+		private final int endIndex;
+
+		public PaginationResult(int page, int pageCount, int startIndex, int endIndex) {
+			this.page = page;
+			this.pageCount = pageCount;
+			this.startIndex = startIndex;
+			this.endIndex = endIndex;
+		}
+
+		public int getPage() {
+			return page;
+		}
+
+		public int getPageCount() {
+			return pageCount;
+		}
+
+		public int getStartIndex() {
+			return startIndex;
+		}
+
+		public int getEndIndex() {
+			return endIndex;
+		}
+
+	}
+
 }
