a0e98befc54e5c5bca4918f14113c68fdd1f4791
kmecpp <kmecpp@gmail.com>
2021-12-30 02:47:13 -0500

    Add more methods to HashSetQueue to get closer to a Queue implementation

--- src/main/java/com/kmecpp/osmium/api/util/lib/HashSetQueue.java
+++ src/main/java/com/kmecpp/osmium/api/util/lib/HashSetQueue.java
@@ -1,35 +1,84 @@
 package com.kmecpp.osmium.api.util.lib;
 
 import java.util.ArrayDeque;
+import java.util.Collection;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.function.Consumer;
 
-public class HashSetQueue<T> {
+public class HashSetQueue<E> {
 
-	protected HashSet<T> queueSet = new HashSet<>();
-	protected ArrayDeque<T> queue = new ArrayDeque<>();
+	protected HashSet<E> queueSet = new HashSet<>();
+	protected ArrayDeque<E> queue = new ArrayDeque<>();
 
 	/**
-	 * Run a consumer on at least <i>limit</i> Players that are removed from the
-	 * queue
+	 * Run a consumer on at least <i>limit</i> elements that are removed from
+	 * the queue
 	 * 
 	 * @param limit
-	 *            the maximum number of players to process
+	 *            the maximum number of elements to process
 	 * @param consumer
-	 *            the player processor
+	 *            the elements processor
 	 */
-	public void process(int limit, Consumer<T> consumer) {
+	public void process(int limit, Consumer<E> consumer) {
 		for (int i = 0; i < limit && !queue.isEmpty(); i++) {
-			T element = queue.poll();
-			queueSet.remove(element);
-			consumer.accept(element);
+			consumer.accept(poll());
 		}
 	}
 
-	public void add(T element) {
+	public boolean add(E element) {
 		if (!queueSet.contains(element)) {
 			queue.add(element);
+			return true;
 		}
+		return false;
+	}
+
+	public E poll() {
+		E element = queue.poll();
+		queueSet.remove(element);
+		return element;
+	}
+
+	public void clear() {
+		queueSet.clear();
+		queue.clear();
+	}
+
+	public int size() {
+		return queue.size();
+	}
+
+	public boolean isEmpty() {
+		return queue.isEmpty();
+	}
+
+	public boolean contains(Object o) {
+		return queue.contains(o);
+	}
+
+	public Iterator<E> iterator() {
+		return queue.iterator();
+	}
+
+	public Object[] toArray() {
+		return queue.toArray();
+	}
+
+	public <T> T[] toArray(T[] a) {
+		return queue.toArray(a);
+	}
+
+	public boolean containsAll(Collection<?> c) {
+		return queue.containsAll(c);
+	}
+
+	public E element() {
+		return queue.element();
+	}
+
+	public E peek() {
+		return queue.peek();
 	}
 
 }
