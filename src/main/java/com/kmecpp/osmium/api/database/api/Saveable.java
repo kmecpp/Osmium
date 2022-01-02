170c2d166c52cee936608cf628958b83d5a824d4
kmecpp <kmecpp@gmail.com>
2022-01-02 16:37:13 -0500

    Update Saveable

--- src/main/java/com/kmecpp/osmium/api/database/api/Saveable.java
+++ src/main/java/com/kmecpp/osmium/api/database/api/Saveable.java
@@ -1,10 +1,9 @@
 package com.kmecpp.osmium.api.database.api;
 
 import com.kmecpp.osmium.Osmium;
-import com.kmecpp.osmium.api.database.MySQLDatabase;
 import com.kmecpp.osmium.api.database.SQLDatabase;
-import com.kmecpp.osmium.api.database.SQLiteDatabase;
 import com.kmecpp.osmium.api.database.TableData;
+import com.kmecpp.osmium.api.logging.OsmiumLogger;
 
 public interface Saveable {
 
@@ -13,30 +12,22 @@ public interface Saveable {
 	}
 
 	default void save(boolean flush) {
-		//		DBTable table = this.getClass().getAnnotation(DBTable.class);
-		//		if (table == null) {
-		//			OsmiumLogger.warn("Classes implementing Savable must be annotated with @" + DBTable.class.getSimpleName());
-		//			return;
-		//		}
 		TableData table = SQLDatabase.getTable(this.getClass());
-		//		if (this.getClass().isAnnotationPresent(MySQLTable.class)) {
-		for (DatabaseType type : table.getTypes()) {
-			if (type == DatabaseType.MYSQL) {
-				MySQLDatabase database = Osmium.getPlugin(this.getClass()).getMySQLDatabase();
-				if (flush || Osmium.isShuttingDown()) {
-					database.replaceInto(this.getClass(), this);
-				} else {
-					database.replaceIntoAsync(this.getClass(), this);
-				}
-			} else if (type == DatabaseType.SQLITE) {
-				SQLiteDatabase database = Osmium.getPlugin(this.getClass()).getSQLiteDatabase();
-				if (flush || Osmium.isShuttingDown()) {
-					database.replaceInto(this.getClass(), this);
-				} else {
-					database.replaceIntoAsync(this.getClass(), this);
-				}
-			}
+		if (table == null) {
+			OsmiumLogger.warn("Classes implementing Savable must be annotated with @" + DBTable.class.getSimpleName());
+			return;
+		}
 
+		for (DatabaseType type : table.getTypes()) {
+			SQLDatabase database = type == DatabaseType.MYSQL
+					? Osmium.getPlugin(this.getClass()).getMySQLDatabase()
+					: Osmium.getPlugin(this.getClass()).getSQLiteDatabase();
+
+			if (flush || Osmium.isShuttingDown()) {
+				database.replaceInto(this.getClass(), this);
+			} else {
+				database.replaceIntoAsync(this.getClass(), this);
+			}
 		}
 	}
 
