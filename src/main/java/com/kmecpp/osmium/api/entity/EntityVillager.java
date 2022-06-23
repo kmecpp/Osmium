61113a5c5850610ca47854bd7231383bbb5d027f
kmecpp <kmecpp@gmail.com>
2022-06-23 11:18:31 -0400

    Move entities package because it's unlikely to be used

---
+++ src/main/java/com/kmecpp/osmium/api/entity/EntityVillager.java
@@ -0,0 +1,5 @@
+package com.kmecpp.osmium.api.entity;
+
+public interface EntityVillager extends EntityLiving {
+
+}
--- src/main/java/com/kmecpp/osmium/api/entity/entities/EntityVillager.java
+++
@@ -1,7 +0,0 @@
-package com.kmecpp.osmium.api.entity.entities;
-
-import com.kmecpp.osmium.api.entity.EntityLiving;
-
-public interface EntityVillager extends EntityLiving {
-
-}
