eab3a1e938a3b379fd6ee1ba8790b44973eff90d
kmecpp <kmecpp@gmail.com>
2024-02-07 20:01:12 -0500

    Update Time class

--- src/main/java/com/kmecpp/osmium/api/util/lib/Time.java
+++ src/main/java/com/kmecpp/osmium/api/util/lib/Time.java
@@ -3,31 +3,59 @@ package com.kmecpp.osmium.api.util.lib;
 public class Time {
 
 	public static long years(int years) {
-		return years * 365 * 24 * 60 * 60 * 1000;
+		return years * 1000 * 60 * 60 * 24 * 365;
 	}
 
 	public static long months(int months) {
-		return months * 7 * 24 * 60 * 60 * 1000;
+		return months * 1000 * 60 * 60 * 24 * 30;
 	}
 
 	public static long weeks(int weeks) {
-		return weeks * 7 * 24 * 60 * 60 * 1000;
+		return weeks * 1000 * 60 * 60 * 24 * 7;
 	}
 
 	public static long days(int days) {
-		return days * 24 * 60 * 60 * 1000;
+		return days * 1000 * 60 * 60 * 24;
 	}
 
 	public static long hours(int hours) {
-		return hours * 60 * 60 * 1000;
+		return hours * 1000 * 60 * 60;
 	}
 
 	public static long minutes(int minutes) {
-		return minutes * 60 * 1000;
+		return minutes * 1000 * 60;
 	}
 
 	public static long seconds(int seconds) {
-		return seconds * 60 * 1000;
+		return seconds * 1000;
+	}
+
+	public static double toYears(long milliseconds) {
+		return (double) milliseconds / 1000 / 60 / 60 / 24 / 365;
+	}
+
+	public static double toMonths(long milliseconds) {
+		return (double) milliseconds / 1000 / 60 / 60 / 24 / 30;
+	}
+
+	public static double toWeeks(long milliseconds) {
+		return (double) milliseconds / 1000 / 60 / 60 / 24 / 7;
+	}
+
+	public static double toDays(long milliseconds) {
+		return (double) milliseconds / 1000 / 60 / 60 / 24;
+	}
+
+	public static double toHours(long milliseconds) {
+		return (double) milliseconds / 1000 / 60 / 60;
+	}
+
+	public static double toMinutes(long milliseconds) {
+		return (double) milliseconds / 1000 / 60;
+	}
+
+	public static double toSeconds(long milliseconds) {
+		return (double) milliseconds / 1000;
 	}
 
 }
